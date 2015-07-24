0517f13f3a295319e13ce025ef3f01314ae28577
Ahmed <ahmed@arayne.com>
2015-07-24 10:25:41 -0400

    add section 1

--- wp-content/themes/mtac/front-page.php
+++ wp-content/themes/mtac/front-page.php
@@ -13,6 +13,38 @@
     <div class="section grey">
         <div class="uk-container uk-container-center">
             <div class="uk-grid">
+                <div class="uk-width-1-1">
+                    <h3 style="text-align:center">About MekTek AutoCare</h3>
+                </div>
+                <div class="uk-width-1-2">
+                    <?php
+                    $page = get_page_by_path( 'section-1' );
+                    echo apply_filters('the_content', $page->post_content);
+                    ?>
+                </div>
+                <div class="uk-width-1-2">
+                    <?php
+                        $page = get_page_by_path( 'section-1' );
+                        echo apply_filters('the_content', $page->post_content);
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section bg1">
+        <div class="uk-container uk-container-center">
+            <div class="uk-grid">
+                <div class="uk-width-1-1">
+                    <p>&nbsp;</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section grey">
+        <div class="uk-container uk-container-center">
+            <div class="uk-grid">
                 <div class="uk-width-1-4">
                     <h1>Services</h1>
                     <a class="service-button" href="">View All Services</a>
